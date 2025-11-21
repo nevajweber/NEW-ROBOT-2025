def4a90fcbfb96816368455eca090c1c51e36082
Maliha654 <amatul.shakur.mohammed19@gmail.com>
2025-11-21 14:20:32 -0700

    Update DriveAutoPID.java

--- DriveAutoPID.java
+++ DriveAutoPID.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "DriveAutoPID", group = "FTC")
+@Autonomous
 public class DriveAutoPID extends LinearOpMode {
 
     private DcMotor FLW, FRW, BLW, BRW;
