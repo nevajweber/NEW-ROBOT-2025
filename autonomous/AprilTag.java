2df6309ca785e9f31306ad7f0dc5bb737e6413f6
nevajweber <nevajweber@gmail.com>
2026-01-26 20:49:41 -0700

    Update AprilTag.java

--- autonomous/AprilTag.java
+++ autonomous/AprilTag.java
@@ -1,4 +1,51 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.robocol.TelemetryMessage;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
 public class AprilTag extends OpMode {
-  
+    private Limelight3A limelight;
+    private IMU imu;
+
+    @Override
+    public void init() {
+      limelight = hardwareMap.get(Limelight3A.class, "limelight");
+      limelight.pipelineSwitch(8); // april tag #11 pipeline
+      imu=hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+           RevHubOrientationOnRobot.USBFacingDirection.FORWARD);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+    }
+
+    @Override
+    public void start() {
+       limelight.start();
+
+
+    }
+
+    @Override
+    public void loop() {
+        YawPitchRollAngles Orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llResult = limelight.getLatestResult();
+        if(llResult != null && llResult.isValid()){
+            Pose3D botPose = llResult.getBotpose_MT2();
+            TelemetryMessage telementry;
+            telementry.addData("Tx", llResult.getTx());
+            telementry.addData("Ty", llResult.getTy());
+            telementry.addData("Ta", llResult.getTa());
+        }
+
+
+    }
+
+
+}
